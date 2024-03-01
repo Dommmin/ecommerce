c4707dc644f0a49ce8ba54f627d2c0204ac61add
Dominik <d.jasinski@ideo.pl>
2024-03-01 14:36:19 +0100

    fix: fix seed bug

--- backend/.tools/scripts/startup.sh
+++ backend/.tools/scripts/startup.sh
@@ -3,6 +3,6 @@
 #composer install --no-dev --optimize-autoloader --no-scripts
 
 php artisan key:generate
-php artisan migrate --seed
+php artisan migrate
 php artisan config:cache
 php artisan serve --host=0.0.0.0 --port=8000
