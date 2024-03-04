28f5db0a5044ab33fc9b3fb8ecc012c6f16bbd9c
Dominik <d.jasinski@ideo.pl>
2024-03-04 14:06:59 +0100

    feat: install, configure and use laravel octane and frankenphp as a server

--- backend/.tools/scripts/startup.sh
+++ backend/.tools/scripts/startup.sh
@@ -5,4 +5,5 @@
 php artisan key:generate
 php artisan migrate
 php artisan config:cache
-php artisan serve --host=0.0.0.0 --port=8000
+#php artisan serve --host=0.0.0.0 --port=8000
+php artisan octane:frankenphp --host=0.0.0.0 --port=8000 --max-requests=1
--- backend/bootstrap/cache/packages.php
+++ backend/bootstrap/cache/packages.php
@@ -17,6 +17,17 @@
       'Horizon' => 'Laravel\\Horizon\\Horizon',
     ),
   ),
+  'laravel/octane' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Octane\\OctaneServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Octane' => 'Laravel\\Octane\\Facades\\Octane',
+    ),
+  ),
   'laravel/pulse' => 
   array (
     'providers' => 
