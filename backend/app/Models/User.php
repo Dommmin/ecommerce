e1bdec9adfb59a5b9b52a6e2461150b876b00336
Dominik <d.jasinski@ideo.pl>
2024-03-21 13:49:00 +0100

    feat: implement MustVerifyEmail

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Orders\Order;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
